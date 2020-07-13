09f2c72ce82a4ac5893014182cc95dd0d935b56b
alienx456 <estebanelias27@gmail.com>
2020-07-13 18:16:07 -0500

    Inferencia funcionando con contenedor

---
+++ src/controlArchvios/src/control.py
@@ -0,0 +1,87 @@
+import os
+import asyncio
+import requests
+
+
+from controlArchivosLinux import ControlArchivosLinux
+
+class Control:
+
+    def __init__(self):
+        self.controlArchivosLinux = ControlArchivosLinux()
+
+        #INFERENCIADOR
+        self.inferenciador_url = os.environ['API_URL']
+        self.inferenciador_url_inicio = os.environ['API_URL_INICIO']
+        self.inferenciador_url_inderenciador = os.environ['API_URL_INFERENCIADOR']
+
+        #TIEMPO DE ESPERA
+        self.espera = int(os.environ['ESPERA'])
+
+        #RUTA
+        self.audio_container_path = os.environ['AUDIO_PATH']
+
+
+
+    async def iniciar(self):
+
+        try:
+
+            print('Iniciando Inferenciador')
+
+            inicioInf = requests.get(self.inferenciador_url + self.inferenciador_url_inicio)
+
+            print("Iniciando inferencias")
+
+            while True:
+
+                lista = [audio for audio in self.controlArchivosLinux.buscarRutasAudios(self.audio_container_path)]
+
+                if len(lista) == 0:
+
+                    print("Lista vacia, función en espera {}...".format(self.espera))
+
+                    await asyncio.sleep(self.espera)
+
+                
+                for audio in lista:
+
+                    self.inferirAudio(audio)
+        
+        except Exception as e:
+
+            print(e)
+                
+
+
+        
+    def inferirAudio(self,audio_nombre):
+
+        print("Realizando inferencia sobre audio : "+audio_nombre)
+
+        try:
+
+            r = requests.post(self.inferenciador_url + self.inferenciador_url_inderenciador, json={"audio_nombre":audio_nombre})
+
+            print("Inferencia completada para audio :"+audio_nombre)
+        
+        except Exception as e:
+
+            print(e)
+            
+            raise
+
+            
+    
+
+
+async def main():
+    
+    control = Control()
+
+    await control.iniciar()
+
+
+
+asyncio.run(main())
+
