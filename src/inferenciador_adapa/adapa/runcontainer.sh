b2ee08e9e10eae16258f03bf33f90ae90d4f779a
alienx456 <estebanelias27@gmail.com>
2020-07-15 15:41:01 -0500

    Test funcionando en host para inferencia adapa

--- src/inferenciador_adapa/adapa/runcontainer.sh
+++ src/inferenciador_adapa/adapa/runcontainer.sh
@@ -1,3 +1,1 @@
-sudo docker run -it -v /home/esteban/Documentos/Tesis/audio_test:/audios adapa-ubuntu:latest /bin/bash
-
-sudo docker run -p 5000:5000 -v /home/esteban/Documentos/Tesis/audio_test:/audios adapa-ubuntu:latest
+sudo docker run -p 5000:5000 -v /home/esteban/Documentos/Tesis/audio_test:/audios --env-file env_vars.env adapa-ubuntu:latest
